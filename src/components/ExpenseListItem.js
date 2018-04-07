bb5b5a6f349e97fa0a95a7f1892f5be861ec368a
R4z1ell <creawshaka@gmail.com>
2018-04-08 01:12:52 +0200

    Setup formatting for amount and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,6 +2,8 @@
 'React.createElement' when parsed, so for this reason we need to import 'React' */
 import React from "react";
 import { Link } from "react-router-dom";
+import moment from "moment";
+import numeral from "numeral";
 
 /* Here below we're DESTRUCTURING the 'props' Object and getting the INDIVIDUAL things OFF of it, and the things
 we want to get off are 'description', 'amount' and 'createdAt', these are pretty much the properties that we 
@@ -13,7 +15,9 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => (
       <h3>{description}</h3>
     </Link>
     <p>
-      {amount} - {createdAt}
+      {numeral(amount / 100).format("$0,0.00")}
+      -
+      {moment(createdAt).format("MMMM Do, YYYY")}
     </p>
   </div>
 );
